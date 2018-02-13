16ac67b3a4745c306b695e8c085b12c61c9b3ba2
Saile Daimwood <slymple@gmail.com>
2018-02-12 22:32:18 -0700

    Strategy A done, Strategy B to be implemented

--- src/StrategyA.java
+++ src/StrategyA.java
@@ -1,5 +1,9 @@
+import java.io.PrintWriter;
+
 /**
- * Runs the random attempt at solving the water jug problem, note that operators 3 and 4 are not included per the instructions.
+ * Runs the random attempt at solving the water jug problem, note that operators
+ * 3 and 4 are not included per the instructions.
+ * 
  * @author Saile Daimwood
  *
  */
@@ -8,49 +12,151 @@ public class StrategyA {
 	private Jug jugB;
 	private int goalA;
 	private int goalB;
-	
-	public StrategyA(int jugA_cap, int jugB_cap, int jugA_init, int jugB_init, int jugA_goal, int jugB_goal) {
+	private PrintWriter out;
+
+	public StrategyA(int jugA_cap, int jugB_cap, int jugA_init, int jugB_init, int jugA_goal, int jugB_goal,
+			PrintWriter out) {
 		jugA = new Jug(jugA_cap, jugA_init);
 		jugB = new Jug(jugB_cap, jugB_init);
-		
+
 		goalA = jugA_goal;
 		goalB = jugB_goal;
-		
+
+		this.out = out;
+
 		run();
 	}
-	
+
+	// Random selection
 	public boolean run() {
-		
+		System.out.println("Strategy A");
+
+		out.println("Strategy A");
+
+		System.out.println("Starting out with a " + jugA.getTotalCapacity() + "-gallon jug and a "
+				+ jugB.getTotalCapacity() + "-gallon jug\t\t --- state: (" + jugA.getCurrentCapacity() + ", "
+				+ jugB.getCurrentCapacity() + ")");
+
+		out.println("Starting out with a " + jugA.getTotalCapacity() + "-gallon jug and a " + jugB.getTotalCapacity()
+				+ "-gallon jug\t\t\t --- state: (" + jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+
+		int counter = 0;
+		while (!solutionReached() && counter < 250) {
+			// generate random number between 0->5
+			int choice = (int) (Math.random() * 6);
+
+			switch (choice) {
+			case 0:
+				fillJugA();
+				break;
+			case 1:
+				fillJugB();
+				break;
+			case 2:
+				emptyJugA();
+				break;
+			case 3:
+				emptyJugB();
+				break;
+			case 4:
+				fillJugAFromB();
+				break;
+			case 5:
+				fillJugBFromA();
+				break;
+			}
+
+			counter++;
+		}
 		return true;
 	}
-	
+
 	// Operator 1
 	public void fillJugA() {
 		jugA.fill();
+		System.out.println("Fill the " + jugA.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+
+		out.println("Fill the " + jugA.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
 	}
-	
+
 	// Operator 2
 	public void fillJugB() {
 		jugB.fill();
+		System.out.println("Fill the " + jugB.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+
+		out.println("Fill the " + jugB.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
 	}
-	
-	//Operator 5
+
+	// Operator 5
 	public void emptyJugA() {
 		jugA.empty();
+		System.out.println("Empty the " + jugA.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+
+		out.println("Empty the " + jugA.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
 	}
-	
-	//Operator 6
+
+	// Operator 6
 	public void emptyJugB() {
 		jugB.empty();
+		System.out.println("Empty the " + jugB.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+
+		out.println("Empty the " + jugB.getTotalCapacity() + "-gallon jug\t\t\t\t\t\t\t\t\t\t --- state: ("
+				+ jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
 	}
-	
-	// This comprises both operators 7 and 9 
+
+	// This comprises both operators 7 and 9
 	public void fillJugAFromB() {
 		jugA.fillFrom(jugB);
+		System.out.println("Pour water from the " + jugA.getTotalCapacity() + "-gallon jug into the "
+				+ jugB.getTotalCapacity() + "-gallon jug\t\t --- state: (" + jugA.getCurrentCapacity() + ", "
+				+ jugB.getCurrentCapacity() + ")");
+
+		out.println("Pour water from the " + jugA.getTotalCapacity() + "-gallon jug into the " + jugB.getTotalCapacity()
+				+ "-gallon jug\t\t --- state: (" + jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
 	}
-	
+
 	// This comprises both operators 8 and 10
 	public void fillJugBFromA() {
 		jugB.fillFrom(jugA);
+		System.out.println("Pour water from the " + jugB.getTotalCapacity() + "-gallon jug into the "
+				+ jugA.getTotalCapacity() + "-gallon jug\t\t --- state: (" + jugA.getCurrentCapacity() + ", "
+				+ jugB.getCurrentCapacity() + ")");
+
+		out.println("Pour water from the " + jugB.getTotalCapacity() + "-gallon jug into the " + jugA.getTotalCapacity()
+				+ "-gallon jug\t\t --- state: (" + jugA.getCurrentCapacity() + ", " + jugB.getCurrentCapacity() + ")");
+	}
+
+	// Pour water from the 7-gallon jug into the 4-gallon jug
+	public boolean solutionReached() {
+		if (goalA == -1 && goalB == -1) {
+			return true;
+		}
+
+		if (goalA == -1) {
+			if (jugB.getCurrentCapacity() == goalB)
+				return true;
+			else
+				return false;
+
+		} else if (goalB == -1) {
+			if (jugA.getCurrentCapacity() == goalA)
+				return true;
+			else
+				return false;
+
+		} else {
+			if (jugA.getCurrentCapacity() == goalA && jugB.getCurrentCapacity() == goalB)
+				return true;
+			else
+				return false;
+
+		}
 	}
 }
--- src/WaterJugProblem.java
+++ src/WaterJugProblem.java
@@ -1,26 +1,30 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class WaterJugProblem {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		// TODO: change new File("inputx.txt"); to new File(args[0]);
 		File inFile = new File("Examples/input1.txt");
 		Scanner in = new Scanner(inFile);
+		PrintWriter out = new PrintWriter(new FileWriter("Examples/output.txt"));
 
 		String jugAInfo = in.nextLine();
 		String jugBInfo = in.nextLine();
 		String initialState = in.nextLine();
 		String goal = in.nextLine();
-		
+
 		in.close();
-		
+
 		// Begin extraction here
 		String[] jugA = jugAInfo.split("Capacity of jug A: ");
 		String[] jugB = jugBInfo.split("Capacity of jug B: ");
 		String[] state = initialState.split("Initial state: ");
 		String[] goalState = goal.split("Goal state: ");
-		
+
 		// After below, jugA and jugB are good to go, just need parsing.
 		jugAInfo = jugA[1];
 		jugBInfo = jugB[1];
@@ -32,15 +36,20 @@ public class WaterJugProblem {
 		int capacityB = Integer.parseInt(jugBInfo);
 		String[] initialStatePair = initialState.split(" ");
 		String[] goalStatePair = goal.split(" ");
-		
+
 		// Parse initial and goal
 		int jugAInitial = Integer.parseInt(initialStatePair[0]);
 		int jugBInitial = Integer.parseInt(initialStatePair[1]);
 		int jugAGoal = Integer.parseInt(goalStatePair[0]);
 		int jugBGoal = Integer.parseInt(goalStatePair[1]);
 
-		StrategyA run_1 = new StrategyA(capacityA, capacityB, jugAInitial, jugBInitial, jugAGoal, jugBGoal);
-		StrategyB run_2 = new StrategyB(capacityA, capacityB, jugAInitial, jugBInitial, jugAGoal, jugBGoal);
+		StrategyA run_1 = new StrategyA(capacityA, capacityB, jugAInitial, jugBInitial, jugAGoal, jugBGoal, out);
+
+		out.println();
+
+		StrategyB run_2 = new StrategyB(capacityA, capacityB, jugAInitial, jugBInitial, jugAGoal, jugBGoal, out);
+
+		out.close();
 
 		System.out.println(run_1 + "\n" + run_2);
 	}
